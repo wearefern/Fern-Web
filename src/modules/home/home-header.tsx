fdcf57b22d0d23fe581c5e7c1e481fe7f3e052ca
shenukadias <shenukadias271@gmail.com>
2026-04-23 16:04:54 +0530

    Update home-header.tsx

--- src/modules/home/home-header.tsx
+++ src/modules/home/home-header.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 
 import { BLOG_PATH } from '~constants/index';
 
-
 import { LAYOUT_ID_HOME_LOGO } from '~ui/atoms/motion';
 import { ThemeToggle } from '~ui/atoms/theme/theme-toggle';
 import { AppHeader } from '~ui/molecules/app-header';
@@ -18,7 +17,7 @@ import { Logo } from '~ui/widgets/logo';
 const NavLink = ({ href, children }: { href: string; children: React.ReactNode }) => (
   <Link
     href={href}
-    className="text-lg font-medium text-ctx-primary-fg-solid hover:opacity-70 transition-opacity"
+    className='text-sm font-medium text-ctx-primary-fg-solid transition-opacity hover:opacity-70 sm:text-base'
   >
     {children}
   </Link>
@@ -26,19 +25,19 @@ const NavLink = ({ href, children }: { href: string; children: React.ReactNode }
 
 const HomeHeader = () => {
   return (
-    <AppHeader innerClassName='flex gap-x-2 justify-between'>
+    <AppHeader mode='compact' innerClassName='flex gap-x-2 justify-between'>
       <Link title='Home' href={'/'}>
-        <Logo layoutId={LAYOUT_ID_HOME_LOGO} />
+        <Logo width={58} layoutId={LAYOUT_ID_HOME_LOGO} />
       </Link>
 
       <MotionConfig transition={{ type: 'spring', duration: 0.5, bounce: 0 }}>
-        <motion.nav layout='position' className='flex items-center gap-x-6'>
+        <motion.nav layout='position' className='flex items-center gap-x-4 sm:gap-x-5'>
           <NavLink href='/#projects'>Projects.</NavLink>
           <NavLink href='/#about'>About.</NavLink>
           <NavLink href={BLOG_PATH}>Blog.</NavLink>
           <NavLink href='#contact'>Contact.</NavLink>
 
-          <ThemeToggle />
+          <ThemeToggle className='p-1 [&_svg]:h-4 [&_svg]:w-4 sm:[&_svg]:h-5 sm:[&_svg]:w-5' />
         </motion.nav>
       </MotionConfig>
     </AppHeader>
