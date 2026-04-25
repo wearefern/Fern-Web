6cd502779a8fabb1c3f6cedcc20591460a5962bc
shenukadias <shenukadias271@gmail.com>
2026-04-25 21:38:02 +0530

    Update blog-logo.tsx

--- src/modules/blog/blog-logo.tsx
+++ src/modules/blog/blog-logo.tsx
@@ -1,7 +1,4 @@
 import { HTMLMotionProps, motion } from 'framer-motion';
-
-import { DynamicWeightOnHoverText } from '~ui/atoms/dynamic-weight-on-hover-text';
-import { Typography } from '~ui/atoms/typography';
 import { Logo } from '~ui/widgets/logo';
 
 import { cn } from '~utils/style';
@@ -10,19 +7,12 @@ import { cn } from '~utils/style';
  * BlogLogo
  * -----------------------------------------------------------------------------------------------*/
 
-interface BlogLogoProps extends HTMLMotionProps<'div'> {
-  trailing?: string;
-}
+interface BlogLogoProps extends HTMLMotionProps<'div'> {}
 
-const BlogLogo = ({ trailing, className, ...rest }: BlogLogoProps) => {
+const BlogLogo = ({ className, ...rest }: BlogLogoProps) => {
   return (
     <motion.div className={cn('flex items-center', className)} {...rest}>
       <Logo />
-
-      <Typography className='ml-1 text-xl tracking-widest sm:text-2xl'>
-        <DynamicWeightOnHoverText text=':insights' />
-        {trailing ? <DynamicWeightOnHoverText text={`:${trailing}`} /> : null}
-      </Typography>
     </motion.div>
   );
 };
