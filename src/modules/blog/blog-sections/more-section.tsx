176b6e67d11d22cfb3edd849d4b024f615e46d60
shenukadias <shenukadias271@gmail.com>
2026-04-25 21:27:39 +0530

    Update more-section.tsx

--- src/modules/blog/blog-sections/more-section.tsx
+++ src/modules/blog/blog-sections/more-section.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import {
-  InlinePlatformRedirectGitHub,
-  InlinePlatformRedirectReddit,
-  InlinePlatformRedirectStackOverflow,
   InlinePlatformRedirectTwitter,
+  InlinePlatformRedirectGitHub,
 } from '~ui/molecules/inline-platform-redirect-with-icon';
 import { SectionContainer } from '~ui/molecules/section/section-container';
 import { SectionHeader } from '~ui/molecules/section/section-header';
@@ -19,17 +17,13 @@ const MoreSection = () => {
     <SectionContainer>
       <SectionHeader
         title='More'
-        subtitle="I'm chronically online (• ◡•) You can find me around."
+        subtitle='Follow Fern across the channels where we publish work and updates.'
       />
 
       <SectionHeadline>
         <span>
-          You can see more of my work on <InlinePlatformRedirectTwitter />, more
-          of my code on <InlinePlatformRedirectGitHub />.
-        </span>
-        <span className='mt-4 block sm:mt-0 sm:inline'>
-          I also help humans on <InlinePlatformRedirectStackOverflow />, and
-          participate in discussions on <InlinePlatformRedirectReddit />
+          Follow product updates on <InlinePlatformRedirectTwitter /> and review
+          engineering work on <InlinePlatformRedirectGitHub />.
         </span>
       </SectionHeadline>
     </SectionContainer>
