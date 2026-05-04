5cf0f5623a70aae742817e1fca4fb4b0c0d63696
Ranuga Fernando <ranugafernando2008@gmail.com>
2026-05-04 15:24:37 +0530

    Update blog-header.tsx
    resolve: fix merge conflict in blog-header

--- src/modules/blog/blog-header.tsx
+++ src/modules/blog/blog-header.tsx
@@ -1,6 +1,12 @@
 'use client';
 
+<<<<<<< Updated upstream
 import { BLOG_PATH } from '~constants/index';
+=======
+import Link from 'next/link';
+
+import { BlogLogo } from '~modules/blog/blog-logo';
+>>>>>>> Stashed changes
 
 import { ThemeToggle } from '~ui/atoms/theme/theme-toggle';
 import { AppHeader } from '~ui/molecules/app-header';
@@ -13,7 +19,13 @@ import { FernLogo } from '../../components/ui/atoms/fern-logo';
 const BlogHeader = () => {
   return (
     <AppHeader innerClassName='flex gap-x-2 justify-between'>
+<<<<<<< Updated upstream
       <FernLogo href={BLOG_PATH} title='Blog' />
+=======
+      <Link title='Fern' href='/'>
+        <BlogLogo />
+      </Link>
+>>>>>>> Stashed changes
 
       <nav className='flex items-center gap-x-2'>
         <ThemeToggle />
